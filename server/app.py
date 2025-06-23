d4e2a86db8edd5b6ca99753b53b1923f35bdaeae
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-23 17:37:27 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -85,14 +85,26 @@ class CheckSession(Resource):
         return {}, 401
 
 class MemberOnlyIndex(Resource):
-    
     def get(self):
-        pass
+        user_id = session.get("user_id")
+        if not user_id:
+            return {"error": "Unauthorized"}, 401
 
+        articles = Article.query.filter_by(is_member_only=True).all()
+        return [article.to_dict() for article in articles], 200
 class MemberOnlyArticle(Resource):
-    
     def get(self, id):
-        pass
+        user_id = session.get("user_id")
+        if not user_id:
+            return {"error": "Unauthorized"}, 401
+
+        article = Article.query.get(id)
+        if not article or not article.is_member_only:
+            return {"error": "Not found"}, 404
+
+        return article.to_dict(), 200
+
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
